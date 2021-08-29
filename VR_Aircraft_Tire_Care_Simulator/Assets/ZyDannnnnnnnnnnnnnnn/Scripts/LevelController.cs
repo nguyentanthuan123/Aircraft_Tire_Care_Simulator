60503490eba796c016c5d6fe436b4a52a2ba7c69
danho191 <danho1241999@gmail.com>
2021-08-29 11:13:59 +0700

    add level controller

--- VR_Aircraft_Tire_Care_Simulator/Assets/ZyDannnnnnnnnnnnnnnn/Scripts/LevelController.cs
+++ VR_Aircraft_Tire_Care_Simulator/Assets/ZyDannnnnnnnnnnnnnnn/Scripts/LevelController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class LevelController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
